3f26f9ad78109d84f6b294651163ff4742fe7a6a
Bastiaan Marinus van de Weerd <bastiaan+git@bm-w.eu>
2024-12-02 08:04:33 -0300

    Initial.

---
+++ src/day00.rs
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Bastiaan Marinus van de Weerd
+
+
+pub(crate) fn part1() -> &'static str {
+	"WIP"
+}
+
+
+pub(crate) fn part2() -> &'static str {
+	"WIP"
+}
+
+
+#[test]
+fn tests() {}
